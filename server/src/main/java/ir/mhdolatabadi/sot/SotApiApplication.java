fbdee7151e09fc3b9b924b02f0c9ea8195d63574
MohammadHossein Dolatabadi <58592602+mhdolatabadi@users.noreply.github.com>
2026-09-24 14:09:33 +0330

    feat(api): add application entry point

---
+++ server/src/main/java/ir/mhdolatabadi/sot/SotApiApplication.java
@@ -0,0 +1,12 @@
+package ir.mhdolatabadi.sot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SotApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SotApiApplication.class, args);
+  }
+}
